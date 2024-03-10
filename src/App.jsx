aecb5d530acec8ba906446cdc57814019d012357
Damian@Vu <minhvdq@gmail.com>
2024-03-10 15:00:07 -0400

    add events to frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
 import { useState, useEffect} from 'react'
 import AuthForm from './components/AuthForm'
+import MainPage from './components/MainPage'
 import loginService from './services/login'
 import customStorage from './services/customStorage'
+import eventServices from './services/events'
+import Togglable from './components/Togglable'
 
 function App() {
   const [curUser, setCurUser] = useState(null)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(null)
+  //const [logged, setLogged] = useState(false)
+  const [events, setEvents] = useState([])
+  const [curUserEvents, setCurUserEvents] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    eventServices.getAll().then(res => {
+      const allEvents = res.data
+      setEvents(res.data)
+      const loggedUser = customStorage.getItem('localUser')
+      if(loggedUser){
+        const lUser = JSON.parse(loggedUser)
+        setCurUser(lUser);
+        const validEvents = allEvents.filter(event => checkEvent(event, lUser.events))
+        console.log(validEvents.length, "is length")
+        setCurUserEvents(validEvents)
+        setLoading(false)
+      }
+      
+    })
+  },[])
+
+  //method to check if an event match as of an user
+  const checkEvent = (eve, eve_ids) => {
+    for(let eve_id of eve_ids){
+      if( eve[0] == eve_id){
+        return true
+      }
+    }
+    return false
+  }
 
   const handlePassword = (event) => {
     setPassword(event.target.value)
@@ -21,11 +55,19 @@ function App() {
     event.preventDefault();
     try{
       const logUser = await loginService.login({email: email, password})
+
+      //get all the events match current user
+      const validEvents = events.filter(event => checkEvent(event, logUser.events))
+      console.log(validEvents.length, "is length")
+      setCurUserEvents(validEvents)
+
+      //handle logging in
       setCurUser(logUser);
       customStorage.setItem('localUser', JSON.stringify(logUser));
       console.log(logUser)
       setUsername('');
       setPassword('');
+
     }catch{
       setError('Wrong username or password')
       setTimeout(() => {
@@ -37,19 +79,39 @@ function App() {
   const handleLogout = (event) => {
     event.preventDefault()
     window.localStorage.clear()
-    setUser(null)
+    setCurUser(null)
+    setEmail('')
+    setPassword('')
   }
 
-  return (
-    <>
+  const loginForm = () => {
+    return(
       <div>
         <AuthForm 
           handleLogin={handleLogin} 
           email={email} password={password} 
           handleEmail={handleEmail} 
           handlePassword={handlePassword} 
+          error={error}
         /> 
-        {error}
+      </div>
+    )
+  }
+
+  const mainPage = () => {
+    console.log('haha', curUserEvents.length)
+    return(
+      <div>
+        <MainPage handleLogout = {handleLogout} events = {curUserEvents} />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div>
+        {curUser == null && loginForm()} 
+        {curUser != null && mainPage()}
       </div>
     </>
   )
